8570755e7e81195e2db665cb93c671bd56b97262
arl <arlatif@pm.me>
2024-06-14 18:35:04 +0100

    make cosmetic updates

--- juice/utils.py
+++ juice/utils.py
@@ -3,25 +3,28 @@ import psycopg
 from psycopg.sql import SQL, Identifier
 
 
-def drop_table(psql_config, table_name='LDZ'):
+def drop_table(psql_config, table_name="LDZ"):
 
     conn = psycopg.connect(**psql_config)
     curr = conn.cursor()
-    curr.execute(SQL('drop table if exists {}').format(Identifier(table_name)))
+    curr.execute(SQL("drop table if exists {}").format(Identifier(table_name)))
     conn.close()
 
 
-def create_ldz_table(psql_config, table_name='LDZ'):
+def create_ldz_table(psql_config, table_name="LDZ"):
     conn = psycopg.connect(**psql_config)
     curr = conn.cursor()
     try:
         curr.execute(
-            SQL("""
+            SQL(
+                """
                 CREATE TABLE {} (
                     id serial PRIMARY KEY,
                     postcode varchar not null,
                     LDZ varchar not null
-                )""").format(Identifier(table_name)))
+                )"""
+            ).format(Identifier(table_name))
+        )
 
     except psycopg.errors.DuplicateTable:
         pass
@@ -29,18 +32,18 @@ def create_ldz_table(psql_config, table_name='LDZ'):
     conn.close()
 
 
-def insert_ldz(psql_config, table_name='LDZ'):
+def insert_ldz(psql_config, table_name="LDZ"):
     conn = psycopg.connect(**psql_config)
     curr = conn.cursor()
 
-    for dn in ['NG', 'NGN', 'SGN', 'WWU']:
+    for dn in ["NG", "NGN", "SGN", "WWU"]:
 
-        with open(f'./ldz/{dn}.csv', 'r') as file:
+        with open(f"./ldz/{dn}.csv", "r") as file:
             data = list(csv.reader(file))[1:]
 
             with curr.copy(
-                    SQL("COPY {} (postcode, LDZ) FROM STDIN").format(
-                        Identifier(table_name))) as copy:
+                SQL("COPY {} (postcode, LDZ) FROM STDIN").format(Identifier(table_name))
+            ) as copy:
                 for row in data:
                     post = [row[0] + row[1], row[2]]
                     try:
