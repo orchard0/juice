e948dcce00705e856b113c379d6661c43350a1ba
arl <arlatif@pm.me>
2024-06-14 18:43:07 +0100

    add retrive_earliest() and update set_energy() and cosmetic changes

--- juice/__init__.py
+++ juice/__init__.py
@@ -4,58 +4,97 @@ from os import path
 from pathlib import Path
 import sys
 import getpass
-from ._psql import create_updates_table
+from ._data import (
+    OCTOPUS_SMART_TARIFF_PRODUCT_CODES,
+    OCTOPUS_SMART_TARIFF_FAMILIES,
+    AGILE_OCTOPUS,
+)
 from datetime import datetime
 
-ENERGY_TYPES = {'gas', 'electricity'}
+ENERGY_TYPES = {"gas", "electricity"}
 
 
 class Juice:
 
-    from ._account import set_account_info, read_account_json, get_account_info, parse_account_information
-    from ._get import get_octopus_products, get_consumption, get_tariffs, get_calorific_values
-    from ._tariffs import remove_tariff, add_bill, add_consumption, add_method, add_method_by_tariff_family, add_method_by_product_code
+    from ._account import (
+        set_account_info,
+        read_account_json,
+        get_account_info,
+        parse_account_information,
+    )
+    from ._get import (
+        get_octopus_products,
+        get_consumption,
+        get_tariffs,
+        get_calorific_values,
+    )
+    from ._tariffs import (
+        remove_tariff,
+        add_bill,
+        add_consumption,
+        add_method,
+        add_method_by_product_family,
+        add_method_by_product_code,
+    )
     from ._bill import run_config, calculate
     from ._print import print_bill, print_method, print_compare, print_checks
+    from ._psql import query_existing_products_tables
+    from ._database import (
+        update_by_product_code,
+        update_by_product_family,
+        update_by_tariff_code,
+        update_existing_products,
+        update,
+        update_database_with_product_code,
+    )
 
-    def __init__(self, API_KEY, ACCOUNT_ID, psql_config=None, LDZ=None, energy_type=None, headers=None) -> None:
+    def __init__(
+        self,
+        API_KEY,
+        ACCOUNT_ID,
+        psql_config=None,
+        LDZ=None,
+        energy_type=None,
+        headers=None,
+    ) -> None:
         self.create_working_dirs()
 
         if psql_config:
             self.psql_config = psql_config
         else:
             self.psql_config = {
-            'user': getpass.getuser(),
-            # 'password': 'password_string',
-            'host': '127.0.0.1',
-            'port': '5432',
-            'dbname': 'utilities',
-            'autocommit': True,
-        }
-
+                "user": getpass.getuser(),
+                # 'password': 'password_string',
+                "host": "127.0.0.1",
+                "port": "5432",
+                "dbname": "utilities",
+                "autocommit": True,
+            }
 
         self.API_KEY = API_KEY
         self.ACCOUNT_ID = ACCOUNT_ID.upper()
         self.ACCOUNT_DATA = self.set_account_info()
-        self.CONSUMPTION, self.AGREEMENTS, self.GSP, queried_ldz = self.parse_account_information(
-            self.ACCOUNT_DATA)
-        
-        self.MOVED_IN_AT = datetime.fromisoformat( self.ACCOUNT_DATA['properties'][0]['moved_in_at'])
+        self.CONSUMPTION, self.AGREEMENTS, self.GSP, queried_ldz = (
+            self.parse_account_information(self.ACCOUNT_DATA)
+        )
+
+        self.ELEC_EARLIEST = self.retrive_earliest(self.AGREEMENTS, "electricity")
+        self.GAS_EARLIEST = self.retrive_earliest(self.AGREEMENTS, "gas")
 
         if LDZ:
             self.LDZ = LDZ
         else:
-             self.LDZ = queried_ldz
+            self.LDZ = queried_ldz
 
         self.energy_type = energy_type
 
         self.calcs = {
-            'gas': {
-                'methods': [],
+            "gas": {
+                "methods": [],
             },
-            'electricity': {
-                'methods': [],
-            }
+            "electricity": {
+                "methods": [],
+            },
         }
 
         self.add_consumption()
@@ -64,13 +103,12 @@ class Juice:
 
         pass
 
-
     @property
     def data(self):
 
         def handle(i):
             if type(i) == pandas.core.frame.DataFrame:
-                return 'PANDAS_DATAFRAME'
+                return "PANDAS_DATAFRAME"
             else:
                 return str(i)
 
@@ -79,42 +117,35 @@ class Juice:
     def set_energy(self, energy_type):
         self.check_energy_type_input(energy_type)
         self.energy_type = energy_type
+
+        if energy_type == "electricity":
+            self.MOVED_IN_AT = self.ELEC_EARLIEST
+        else:
+            self.MOVED_IN_AT = self.GAS_EARLIEST
         pass
 
-    def update(self, force_refresh=False):
+    @staticmethod
+    def retrive_earliest(agreements, energy_type):
 
-        print(f'Updating {self.ACCOUNT_DATA['number']}.')
-
-        create_updates_table(self.psql_config)
-
-        for consumption in self.CONSUMPTION:
-            self.get_consumption(self.psql_config, self.API_KEY,
-                                 account_id=self.ACCOUNT_ID,
-                                 force_refresh=force_refresh,
-                                 headers=self.headers,
-                                 **consumption)
-
-        for tariff in self.AGREEMENTS:
-            res = {key: tariff[key] for key in {'energy_type', 'tariff_code'}}
-            self.get_tariffs(self.psql_config, force_refresh=force_refresh, headers=self.headers, **res)
-
-        self.get_octopus_products(self.psql_config, self.headers)
-
-        if self.calcs['gas']['consumption_dbs']:
-            if not self.LDZ:
-                raise ValueError('There were gas consumption databases found but no LDZ. Please add it to the Juice constructor.')
-            for x in self.ACCOUNT_DATA['properties']:
-                self.get_calorific_values(self.psql_config, x['moved_in_at'], self.LDZ, headers=self.headers)
-
-        print('Completed update.')
-        print('='*15)
-
+        filtered = filter(lambda n: n["energy_type"] == energy_type, agreements)
+        try:
+            return sorted(
+                filtered,
+                key=lambda n: n["valid_from"],
+            )[
+                0
+            ]["valid_from"]
+        except:
+            return None
 
     def search_method(self, energy_type, name):
 
         method = list(
-            filter(lambda n: n['name'].lower() == name.lower(),
-                   self.calcs[energy_type]['methods']))
+            filter(
+                lambda n: n["name"].lower() == name.lower(),
+                self.calcs[energy_type]["methods"],
+            )
+        )
 
         if len(method) == 0:
             return None
@@ -134,5 +165,5 @@ class Juice:
     @staticmethod
     def create_working_dirs():
         script_dir = path.dirname(sys.argv[0])
-        accounts_dir = path.join(script_dir, 'Accounts')
+        accounts_dir = path.join(script_dir, "Accounts")
         Path(accounts_dir).mkdir(parents=True, exist_ok=True)
